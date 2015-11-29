c1ff2d8269ec9a334cfb4014072bcca18feb88b9
Fifty pennies <fiftypenniez@gmail.com>
2015-11-29 18:35:31 +0100

    Update functions.php

--- assets/functions.php
+++ assets/functions.php
@@ -2,36 +2,32 @@
 
 
 
-	class Server 
+class Server
+{
+    function getStatus()
     {
-    	function getStatus() 
-        {
-        	$host 		= $_SERVER['SERVER_ADDR'];
-        	$timeout 	= '10';
+        $host    = $_SERVER['SERVER_ADDR'];
+        $timeout = '10';
         
-        	// check web server status
-        	try
-            {
-            	$fsock = fsockopen($host, '80', $errno, $errstr, $timeout);
-            	
-            	if (!$fsock)
-                {
-                	// web server down?
-                	$webserver = 'FALSE';
-                } else {
-                	 // web server up!
-                	 $webserver = 'TRUE';
-                }
+        // check web server status
+        try {
+            $fsock = fsockopen($host, '80', $errno, $errstr, $timeout);
             
-            
-            	catch(Exception $error) 
-                {
-  					echo 'Exception caught: ' . $error->getMessage();
-				}
-            
-            	return $webserver;
+            if (!$fsock) {
+                // web server down?
+                $webserver = 'FALSE';
+            } else {
+                // web server up!
+                $webserver = 'TRUE';
             }
+            
         }
+        catch (Exception $error) {
+            echo 'Exception caught: ' . $error->getMessage();
+        }
+        return $webserver;
+        
     }
+}
 
 ?>    
