017c49a18d300b2a0eb8a20f3bc7d0e623c89a1f
Fifty pennies <fiftypenniez@gmail.com>
2015-11-29 18:26:01 +0100

    fixed so it uses template engine

--- install/index.php
+++ install/index.php
@@ -1,57 +1,16 @@
-<!DOCTYPE HTML>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-    <!-- CSS files -->
-    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Roboto:400,700" rel="stylesheet">
- 	<link href="../css/semantic.min.css" rel="stylesheet">
+<?php
+		require_once('../assets/global.php');
 
- 	<!-- JS files -->
- 	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
- 	<script type="text/javascript" src="../js/semantic.min.js"></script>
-
- 	</head>
- 	<body>
-    
-    
-<div class="ui text container">
-      <h1 class="ui dividing header" style="margin-top:20px">Install Awards Manager</h1>
-
-      <div class="ui icon message inverted">
-      	<i class="notched warning sign icon"></i>
-      	<div class="content">
-  <div class="header">
-   	Before installing...
-  </div>
-  <p>We need to make sure that you have a few extensions enabled so that Awards Manager runs without any problems! :)</p>
-  	<div class="ui list smalltext">
-  	  <a class="item">
-    <i class="right triangle icon"></i>
-    <div class="content">
-      <div class="header">MySQLi</div>
-      <div class="description">We've added support for use of MySQL Improved (just like MyBB).</div>
-    </div>
-  </a>
-
-    	  <a class="item inverted">
-    <i class="right triangle icon"></i>
-    <div class="content">
-      <div class="header">Server Health</div>
-      <div class="description">We've added support for server monitoring under the Server tab.</div>
-    </div>
-  </a>
-    	  <a class="item">
-    <i class="right triangle icon"></i>
-    <div class="content">
-      <div class="header">Uninstall</div>
-      <div class="description">We've added support for uninstalling the application.</div>
-    </div>
-  </a>
-</div>
-</div>
-</div>
-
-      <h3 class="first ui dividing header">Miscellaneous</h3>
+		$var = new Template("assets/templates/installer.php");
+		$var->set("title", "Awards Manage Installerr");
+		// CSS
+		$var->addCSS("https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css");
+		$var->addCSS("https://fonts.googleapis.com/css?family=Roboto:400,700");
+		$var->addCSS("assets/css/semantic.min.css");
+		// JS
+		$var->addJS("https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js");
+		$var->addJS("https://cdnjs.cloudflare.com/ajax/libs/jquery-cookie/1.4.1/jquery.cookie.min.js");
+		$var->addJS("assets/js/semantic.min.js");
+		$var->addJS("assets/js/popups.js");
+		echo $var->output();
+?>
